28c398e72b02ee5d3e8feb700db8ed0f25228848
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-11-21 15:36:29 -0800

    completed trending page & search function

--- crypto-tracker/src/stores/homeStore.js
+++ crypto-tracker/src/stores/homeStore.js
@@ -1,30 +1,56 @@
 import create from "zustand";
 import axios from "axios";
+import debounce from "../helpers/debounce";
 
 const homeStore = create((set) => ({
-    coins: [],
-    query: "",
+  coins: [],
+  trending: [],
+  query: "",
 
-    setQuery: (e) => {
-        set({query: e.target.value})
-    },
+  setQuery: (e) => {
+    set({ query: e.target.value });
+    homeStore.getState().searchCoins();
+  },
+
+  searchCoins: debounce(async () => {
+    const { query, trending } = homeStore.getState();
+
+    if (query.length > 2) {
+      const res = await axios.get(
+        `https://api.coingecko.com/api/v3/search?query=${query}`
+      );
+      const coins = res.data.coins.map((coin) => {
+        return {
+          name: coin.name,
+          image: coin.large,
+          id: coin.id,
+        };
+      });
+
+      set({
+        coins: coins,
+      });
+    } else {
+        set({coins: trending})
+    }
+  }, 500),
 
   fetchCoins: async () => {
     const res = await axios.get(
-      "https://api.coingecko.com/api/v3/search/trending")
+      "https://api.coingecko.com/api/v3/search/trending"
+    );
 
-      const coins = res.data.coins.map(coin => {
-        return {
+    const coins = res.data.coins.map((coin) => {
+      return {
         name: coin.item.name,
         image: coin.item.large,
         id: coin.item.id,
-        priceBtc: coin.item.price_btc
-        }
-      })
+        priceBtc: coin.item.price_btc,
+      };
+    });
 
-      set({coins: coins})
-
-  }
+    set({ coins: coins, trending: coins });
+  },
 }));
 
 export default homeStore;
