404775ec7f61f1bbc23c5531f1a2b7ad8054bf98
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-11-21 16:24:03 -0800

    fetched market chart api for show

--- crypto-tracker/src/stores/showStore.js
+++ crypto-tracker/src/stores/showStore.js
@@ -1,11 +1,25 @@
 import create from "zustand";
 import axios from "axios";
 
-
 const showStore = create((set) => ({
-    fetchData: () => {
-        console.log("hey");
-    },
+    graphData: [],
+  fetchData: async (id) => {
+    const res = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`
+    );
+        const graphData = res.data.prices.map(price => {
+            const [timestamp, p] = price;
+            const date = new Date(timestamp).toLocaleDateString("en-us")
+            return  {
+                Date: date,
+                Price: p,
+              };
+        });
+
+        set({ graphData });
+
+        console.log(res.data)
+  },
 }));
 
 export default showStore;
