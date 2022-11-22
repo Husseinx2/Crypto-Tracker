bfef10f29aecd23ee97122dbdcb84c5a6de27c31
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-11-21 22:14:25 -0800

    fetched more apis using coingecko

--- crypto-tracker/src/stores/showStore.js
+++ crypto-tracker/src/stores/showStore.js
@@ -2,23 +2,31 @@ import create from "zustand";
 import axios from "axios";
 
 const showStore = create((set) => ({
-    graphData: [],
+  graphData: [],
+
   fetchData: async (id) => {
-    const res = await axios.get(
-      `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`
-    );
-        const graphData = res.data.prices.map(price => {
-            const [timestamp, p] = price;
-            const date = new Date(timestamp).toLocaleDateString("en-us")
-            return  {
-                Date: date,
-                Price: p,
-              };
-        });
+    // eslint-disable-next-line 
+    const [graphRes, dataRes] = await Promise.all([
+      axios.get(
+        `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=usd&days=121`
+      ),
+      axios.get(
+        `https://api.coingecko.com/api/v3/coins/${id}?localization=false&market_data=true`
+      ),
+    ]);
 
-        set({ graphData });
+    const graphData = graphRes.data.prices.map((price) => {
+      const [timestamp, p] = price;
+      const date = new Date(timestamp).toLocaleDateString("en-us");
+      return {
+        Date: date,
+        Price: p,
+      };
+    });
 
-        console.log(res.data)
+    // console.log(dataRes);
+    set({ graphData });
+
   },
 }));
 
