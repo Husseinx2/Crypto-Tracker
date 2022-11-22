27d592ddab3c41f15b3b39e6ede0b3124fa3dd25
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-11-21 22:19:01 -0800

    adding more data to show, not rending data correctly

--- crypto-tracker/src/pages/Show.jsx
+++ crypto-tracker/src/pages/Show.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import showStore from "../stores/showStore";
-import React, { PureComponent } from "react";
+import React from "react";
 import {
   AreaChart,
   Area,
@@ -8,53 +8,52 @@ import {
   YAxis,
   CartesianGrid,
   Tooltip,
-  ResponsiveContainer,
 } from "recharts";
 
-const data = [
-  {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
+// const data = [
+//   {
+//     name: "Page A",
+//     uv: 4000,
+//     pv: 2400,
+//     amt: 2400,
+//   },
+//   {
+//     name: "Page B",
+//     uv: 3000,
+//     pv: 1398,
+//     amt: 2210,
+//   },
+//   {
+//     name: "Page C",
+//     uv: 2000,
+//     pv: 9800,
+//     amt: 2290,
+//   },
+//   {
+//     name: "Page D",
+//     uv: 2780,
+//     pv: 3908,
+//     amt: 2000,
+//   },
+//   {
+//     name: "Page E",
+//     uv: 1890,
+//     pv: 4800,
+//     amt: 2181,
+//   },
+//   {
+//     name: "Page F",
+//     uv: 2390,
+//     pv: 3800,
+//     amt: 2500,
+//   },
+//   {
+//     name: "Page G",
+//     uv: 3490,
+//     pv: 4300,
+//     amt: 2100,
+//   },
+// ];
 
 export default function Show() {
   const store = showStore();
@@ -64,8 +63,18 @@ export default function Show() {
     store.fetchData(params.id);
   }, []);
 
+//   if (!store.data) return <></>;
+if (store.data) {
+    return <p>Loading Data...</p>;
+}
   return (
     <div>
+      <header>
+        {/* <img src={store.data.image.large} alt="" /> */}
+        <h2>
+          {/* {store.data.name} ({store.data.symbol}) */}
+        </h2>
+      </header>
       <AreaChart
         width={500}
         height={400}
@@ -83,6 +92,32 @@ export default function Show() {
         <Tooltip />
         <Area type="monotone" dataKey="Price" stroke="#8884d8" fill="#8884d8" />
       </AreaChart>
+      <div>
+        <h4>Market Cap Rank</h4>
+        {/* <span>{store.data.market_data.market_cap_rank}</span> */}
+      </div>
+      <div>
+        <h4>24h High</h4>
+        {/* <span>${store.data.market_data.high_24h.usd}</span> */}
+      </div>
+      <div>
+        <h4>24h Low</h4>
+        {/* <span>${store.data.market_data.low_24h.usd}</span> */}
+      </div>
+      <div>
+        <h4>Circulating Supply</h4>
+        {/* <span>${store.data.market_data.circulating_supply}</span> */}
+      </div>
+      <div>
+        <h4>Current Price</h4>
+        {/* <span>${store.data.market_data.current_price.usd}</span> */}
+      </div>
+      <div>
+        <h4>1y Change</h4>
+        <span>
+          {/* {store.data.market_data.price_change_percentage_1y.toFixed(2)}% */}
+        </span>
+      </div>
     </div>
   );
 }
