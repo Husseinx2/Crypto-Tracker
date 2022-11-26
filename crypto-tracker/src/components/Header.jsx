467d8e50e9682e13b284d94bdc118d72eb0738ed
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2022-11-25 17:55:58 -0800

    styling header more

--- crypto-tracker/src/components/Header.jsx
+++ crypto-tracker/src/components/Header.jsx
@@ -10,14 +10,13 @@ export default function Header() {
     <Navbar className="navbar" bg="light" expand="lg" sticky="top">
       <Container>
         <Navbar.Brand className="Navbar-Brand" href="/"> <img src={Logo} className="Navbar-logo" alt="" /></Navbar.Brand>
-        <Navbar.Brand className="Navbar-Brand" href="/"> Crypto Tracker</Navbar.Brand>
+        <Navbar.Brand className="Navbar-Brand" href="/"> <bdo className="white">"Crypto</bdo> Tracker"</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto" >
-
             <Nav.Link href="/" className="NavLink" > <i className="fa fa-home fa-lg"></i> Home </Nav.Link>
-            <Nav.Link href="/" className="NavLink" > <i className="fa fa-info fa-lg"></i> About us </Nav.Link>
-            <Nav.Link href="/" className="NavLink"> <i className="fa fa-star fa-lg" ></i> featured</Nav.Link>
+            <Nav.Link href="/" className="NavLink" > <i className="fa fa-info fa-lg"></i> About Us </Nav.Link>
+            <Nav.Link href="/" className="NavLink"> <i className="fa fa-star fa-lg" ></i> Featured</Nav.Link>
             <Nav.Item>    </Nav.Item>
 
           </Nav>
