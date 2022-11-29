e997c7e53f4669832b6d372ce083f288e81b7f85
Hussein Hussein <husseinwkc@gmail.com>
2022-11-29 04:06:39 -0500

    tweets added -note, will edit card when decision is made on keeping card

--- crypto-tracker/src/components/News.jsx
+++ crypto-tracker/src/components/News.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-
+import Container from "react-bootstrap/esm/Container";
+import TweetCard from "react-tweet-card";
+/// api:Returns the last 50 timeline tweets from the official Twitter profile for a given coin.
 const News = () => {
   const [data, setData] = useState(null);
   const url = "https://api.coinpaprika.com/v1/coins/btc-bitcoin/twitter";
@@ -18,10 +20,36 @@ const News = () => {
   if (!data) return null;
 
   return (
-  <div className="news">
-    <h1>Twitter</h1>
-  </div>
-)
+    <div className="news">
+      <h1>Twitter</h1>
+      <Container>
+        <TweetCard
+        engagement={{
+          retweets:data[0].retweet_count,
+          likes: data[0].like_count
+
+        }}
+          author={{
+            username: data[0].user_name,
+            image: data[0].user_image_link,
+            
+          }}
+          tweet={data[0].status}
+          time={data[0].date}
+          
+          permalink={data[0].status_link}
+          colors={{
+            primary: 'black',
+            secondary: '#08004d',
+            accent: '#ffffd8',
+            
+            
+          }}
+          
+        ></TweetCard>
+      </Container>
+    </div>
+  );
 };
 
 export default News;
