7d570d9a5a37c01370a016918444fe257887bbf1
Hussein Hussein <husseinwkc@gmail.com>
2022-12-07 20:52:21 -0500

    route added for profile.jsx

--- crypto-tracker/src/index.js
+++ crypto-tracker/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Show from "./pages/Show";
+import Profile from "./pages/Profile";
 import "./style.scss";
 import 'font-awesome/css/font-awesome.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,6 +14,7 @@ root.render(
     <Routes>
       <Route index element={<Home />} />
       <Route path="/:id" element={<Show />} />
+      <Route path="/Profile" element={<Profile/>}/>
     </Routes>
   </BrowserRouter>
 );
