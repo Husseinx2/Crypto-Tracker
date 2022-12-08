b8a4208a4bfc5d4f8502fe82d9a9401b8b62bde8
Hussein Hussein <husseinwkc@gmail.com>
2022-12-07 20:49:59 -0500

    Profile page where our info is

---
+++ crypto-tracker/src/pages/Profile.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+
+import About from "../components/About";
+
+export default function Profile() {
+  return (
+    <div>
+      <Header />
+      <About />
+
+    </div>
+  );
+}
